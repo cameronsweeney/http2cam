265075f89c60b9cdb89ecc0a1238e95881301b67
cameronsweeney <81322885+cameronsweeney@users.noreply.github.com>
2022-09-19 20:02:49 -0400

    fixing hosts.py

--- config/hosts.py
+++ config/hosts.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host('', 'config.urls', name='homepage_host'),
+    host('www', 'config.urls', name='homepage_host'),
     host('pokedex', 'pokedex.urls', name='pokedex'),
     host('latb22', settings.ROOT_URLCONF, name='latb22'),
 )
