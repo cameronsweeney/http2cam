4889edf8e7e286ae1eb828b49dba17a62abfb2e9
cameronsweeney <81322885+cameronsweeney@users.noreply.github.com>
2022-09-19 20:09:34 -0400

    adding logging

--- config/settings.py
+++ config/settings.py
@@ -205,3 +205,36 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 SITE_ID = 1
 ROOT_HOSTCONF = 'config.hosts'
 DEFAULT_HOST = 'homepage_host'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
